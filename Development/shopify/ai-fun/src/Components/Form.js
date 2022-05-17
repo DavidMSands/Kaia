cf5e6ea1e491f1397b21e5d34c7e4911b21d38f2
David <Davidmaxsands@gmail.com>
2022-05-16 19:06:09 -0600

    created componenets and setup fetch

---
+++ Development/shopify/ai-fun/src/Components/Form.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+const KEY = process.env.REACT_APP_OPENAI_SECRET
+
+function Form() {
+    const [prompt, setPrompt] = useState()
+
+    const kaiaSubmit = (e) => {
+        e.preventDefault()
+        const data = {
+            prompt: prompt,
+            temperature: 0.5,
+            max_tokens: 64,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
+
+        fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${KEY}`,
+            },
+            body: JSON.stringify(data),
+            })
+            .then(res => res.json())
+            .then(response => console.log(response))
+            setPrompt()
+    }
+
+
+  return (
+    <div id='form'>
+        <form onSubmit={kaiaSubmit}>
+            <label>Send a prompt to Kaia: </label>
+            <div id='ask-kaia'>
+                <textarea rows="10" cols="50" placeholder='Write a haiku about rice...' value={prompt} onChange={(e) => setPrompt(e.target.value)}></textarea>
+            </div>
+            <input type='submit' value="Send" className='new-prompt-submit'></input>
+        </form>
+    </div>
+  )
+}
+
+export default Form
---
+++ Development/shopify/ai-fun/src/Components/Responses.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Responses() {
+  return (
+    <div>
+        <h2>Kaia's Responses</h2>
+    </div>
+  )
+}
+
+export default Responses
